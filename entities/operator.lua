85fa2deb5b22e83c05ae236ba80db90f28e4927e
pevzi <pevzi23@gmail.com>
2015-09-26 00:57:58 +0700

    Make it possible to run several coroutines at the same time
    Isn't really useful here but still architectually better?

--- entities/operator.lua
+++ entities/operator.lua
@@ -2,6 +2,16 @@ local class = require "libs.middleclass"
 local Stateful = require "libs.stateful"
 local cron = require "libs.cron"
 
+local function makeContinue()
+    local co = coroutine.running()
+
+    local function continue()
+        coroutine.resume(co)
+    end
+
+    return continue, co
+end
+
 local Show = {}
 
 function Show:enteredState()
@@ -41,38 +51,48 @@ function Operator:initialize(field, fieldView, swapDelay, swapDuration, rounds, 
     self.simultaneous = simultaneous
     self.ballDelay = ballDelay
 
+    self.timers = {}
+
     self:gotoState("Show")
 end
 
 function Operator:runCoroutine(body)
-    self.continue = coroutine.wrap(body)
-    self.continue(self.continue)
+    local co = coroutine.create(body)
+    coroutine.resume(co)
 end
 
 function Operator:sleep(duration)
-    self.timer = cron.after(duration, self.continue)
+    local continue, co = makeContinue()
+
+    local timer = cron.after(duration, function ()
+        self.timers[co] = nil
+        continue()
+    end)
+
+    self.timers[co] = timer
+
     coroutine.yield()
 end
 
 function Operator:showBalls()
-    self.fieldView:showBalls(true):oncomplete(self.continue)
+    self.fieldView:showBalls(true):oncomplete(makeContinue())
     coroutine.yield()
 end
 
 function Operator:hideBalls()
-    self.fieldView:showBalls(false):oncomplete(self.continue)
+    self.fieldView:showBalls(false):oncomplete(makeContinue())
     coroutine.yield()
 end
 
 function Operator:swap()
     local swappedPairs = self.field:swap(self.simultaneous)
-    self.fieldView:swap(swappedPairs, self.swapDuration):oncomplete(self.continue)
+    self.fieldView:swap(swappedPairs, self.swapDuration):oncomplete(makeContinue())
     coroutine.yield()
 end
 
 function Operator:update(dt)
-    if self.timer then
-        self.timer:update(dt)
+    for _, timer in pairs(self.timers) do
+        timer:update(dt)
     end
 end
 
