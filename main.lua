2f8a7be77011a5ad22f92b19bdab3e6e86b44143
pevzi <pevzi23@gmail.com>
2016-03-02 16:05:40 +0700

    Add a _G metatable

--- main.lua
+++ main.lua
@@ -1,3 +1,13 @@
+setmetatable(_G, {
+    __index = function (t, k)
+        error(("attempt to access an undefined global variable '%s'"):format(k), 2)
+    end,
+
+    __newindex = function (t, k, v)
+        error(("attempt to assign to an undefined global variable '%s'"):format(k), 2)
+    end
+})
+
 local Game = require "game"
 
 local game
