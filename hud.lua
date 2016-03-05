01a99c57c6d150d6bb141456e84dd9bfb666c88c
pevzi <pevzi23@gmail.com>
2016-03-05 16:45:50 +0700

    Some HUD cleanup

--- hud.lua
+++ hud.lua
@@ -13,23 +13,22 @@ function HUD:initialize(game, height)
     self.width = love.graphics.getWidth()
     self.height = height
 
-    local scale = self.height * 0.6 / res.images.question:getHeight()
-
-    self.query = {x = self.width / 2 - res.images.question:getWidth() * scale / 2,
-        y = self.height / 2 - res.images.question:getHeight() * scale / 2,
-        scale = scale,
-        color = {255, 255, 255, 0}}
+    self.query = {x = self.width / 2, y = self.height / 2,
+        ox = res.images.question:getWidth() / 2,
+        oy = res.images.question:getHeight() / 2,
+        scale = self.height * 0.6 / res.images.question:getHeight(),
+        color = {255, 255, 255, 0},
+        icon = "question"
+    }
 
     self.tweens = flux.group()
-
-    self.icon = "question"
 end
 
 function HUD:askColor(color)
     self.query.color = {color[1], color[2], color[3], 0}
     self.tweens:to(self.query.color, 0.2, {[4] = 255})
 
-    self.icon = "question"
+    self.query.icon = "question"
 end
 
 function HUD:hide()
@@ -37,11 +36,11 @@ function HUD:hide()
 end
 
 function HUD:sayCorrect()
-    self.icon = "correct"
+    self.query.icon = "correct"
 end
 
 function HUD:sayIncorrect()
-    self.icon = "incorrect"
+    self.query.icon = "incorrect"
 end
 
 function HUD:update(dt)
@@ -51,8 +50,11 @@ end
 function HUD:draw()
     love.graphics.translate(self.x, self.y)
 
-    love.graphics.setColor(self.query.color)
-    love.graphics.draw(res.images[self.icon], self.query.x, self.query.y, 0, self.query.scale)
+    local query = self.query
+
+    love.graphics.setColor(query.color)
+    love.graphics.draw(res.images[query.icon], query.x, query.y, 0,
+        query.scale, query.scale, query.ox, query.oy)
 end
 
 return HUD
